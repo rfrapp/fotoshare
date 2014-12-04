077d210d81f4f84cece67b853107decd200c1565
Ryan Frappier <ryan@Ryans-MacBook-Pro-2.local>
2014-12-04 14:45:01 -0600

    Add stuff

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -1,5 +1,5 @@
 class UserMailer < ActionMailer::Base
-  default from: "noreply@fotoshare.com"
+  default from: "fotoshare123@gmail.com"
 
   # Subject can be set in your I18n file at config/locales/en.yml
   # with the following lookup:
